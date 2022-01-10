babfddff1d4da40835418ede1ad3e284522e4075
I'm Unix <ferdi@ferdiwardiyan.eu.org>
2022-01-10 18:44:15 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
 echo "nameserver 2001:67c:2960:5353:5353:5353:5353:5353" > /etc/resolv.conf
 echo "nameserver 2001:67c:2960:6464:6464:6464:6464:6464" >> /etc/resolv.conf
+apt-get update
+apt-get upgrade -y
+apt-get install curl git nano -y
