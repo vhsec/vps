02aa9692d340bc7d45300271996704a616f6f6f9
I'm Unix <ferdi@ferdiwardiyan.eu.org>
2022-04-01 10:30:55 +0700

    Create nheq.sh

---
+++ nheq.sh
@@ -0,0 +1,5 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+cd nheqminer
+./nheqminer -v -l na.luckpool.net:3956 -u RH82h7vWtKd3vjj7i76WU5ejVtxJzrVBRb.jso -p 'd=16,xn=1,hybrid' -t 8
